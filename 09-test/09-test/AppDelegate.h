022be68e40689ee6da9edb022d97cead026555e3
OYforQX <admin@admindemacbook-pro.local>
2016-04-14 19:38:38 +0800

    开始测试

---
+++ 09-test/09-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  09-test
+//
+//  Created by admin on 16/4/14.
+//  Copyright © 2016年 OY-ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 09-test/09-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  09-test
+//
+//  Created by admin on 16/4/14.
+//  Copyright © 2016年 OY-ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
